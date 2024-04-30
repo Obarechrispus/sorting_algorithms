16b0e4092a666a3a7be897132d77db959f6e1f75
Obarechrispus <obarechrispus@gmail.com>
2024-04-30 09:20:08 -0700

    task2

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,3 +9,32 @@
  *
  */
 void insertion_sort_list(listint_t **list)
+{
+
+	listint_t *current = (*list)->next;
+	listint_t *temp = current;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		
+		while (temp->prev != NULL && temp ->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
+			else
+				*list = temp;
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}	
