1f2d436b2b7cea3c9f1ebcc89052ac640cd18982
Obarechrispus <obarechrispus@gmail.com>
2024-04-30 05:55:55 -0700

    update

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+#include "stdlib.h"
+/*
+ *insertion_sort_list - sorts a doubly linked of intergers in ascending order
+ *
+ *
+ *
+ *list: The list to be sorted
+ *
+ */
+void insertion_sort_list(listint_t **list)
